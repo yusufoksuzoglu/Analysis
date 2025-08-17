ba5a64cfc585e57fe750f8694e42ec2c312b2ff1
Yusuf <122007538+yusufoksuzoglu@users.noreply.github.com>
2025-08-17 16:10:38 +0300

    insert some values and explore new things on sql company.sql

--- company.sql
+++ company.sql
@@ -73,3 +73,34 @@ create table dependent(
 )engine=innodb;
 insert into employee(Bdate, Fname, Minit, Lname, Address, Sex, Salary) 
     values('2002-11-01', 'Yusuf', NULL, 'ÖKSÜZOĞLU', 'İstanbul', 'E', 15000);
+insert into employee(Bdate, Fname, Minit, Lname, Address, Sex, Salary) values('2006-11-01', 'Ömer', 'F', 'ÖKSÜZOĞLU', 'İstanbul','E', 18000); 
+insert into employee(Bdate, Fname, Minit, Lname, Address, Sex, Salary) values('1975-11-01', 'Vedat', NULL, 'ÖKSÜZOĞLU', 'İstanbul','E', 50000); 
+insert into employee(Bdate, Fname, Minit, Lname, Address, Sex, Salary) values('2002-06-18', 'Erdem', NULL, 'ALTUN', 'İstanbul','E', 30000); 
+insert into employee(Bdate, Fname, Minit, Lname, Address, Sex, Salary) values('2001-01-16', 'Ömer', 'F', 'AKSU', 'İstanbul','E', 5000); 
+insert into department(Name, Number) values('TK', 01); 
+insert into department(Name, Number) values('TGS', 02); 
+insert into department(Name, Number) values('İGA', 03); 
+insert into manages(Ssn, Name, Number, StartDate) values(01, 'TK', 01, '2025-09-01'); 
+insert into manages(Ssn, Name, Number, StartDate) values(02, 'İGA', 03, '2029-09-01'); 
+insert into manages(Ssn, Name, Number, StartDate) values(03, 'TGS', 02, '2004-09-01'); 
+insert into manages(Ssn, Name, Number, StartDate) values(04, 'TK', 01, '2026-09-01')
+insert into manages(Ssn, Name, Number, StartDate) values(05, 'İGA', 3, '2025-09-01');
+SELECT 
+    e.Bdate, 
+    e.Fname, 
+    e.Minit,
+    e.Lname, 
+    e.Address,
+    e.Sex,
+    e.Salary,
+    d.Name AS DepartmentName,
+    d.Number AS DepartmentNumber, 
+    m.StartDate 
+    FROM Manages m JOIN Employee e ON e.Ssn = m.Ssn 
+    JOIN Department d ON d.Name = m.Name AND d.Number = m.Number;
+select employee.Fname, employee.Minit, employee.Lname, 
+    dense_rank() over(order by salary desc) as 'rank' from employee;
+SELECT department.name, department.Number, manages.Name, manages.Number 
+    from department left join manages 
+    on department.Name = manages.Name and department.Number = manages.Number;
+
